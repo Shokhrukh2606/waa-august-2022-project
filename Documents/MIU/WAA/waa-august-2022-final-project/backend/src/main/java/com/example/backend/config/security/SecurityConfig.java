24c23821c86f16e5f34008f3f90625bfaecf266e
Javokhir Mamadiyarov <frodonumb@gmail.com>
2022-08-07 10:21:38 -0500

    Comments on Students realized

--- Documents/MIU/WAA/waa-august-2022-final-project/backend/src/main/java/com/example/backend/config/security/SecurityConfig.java
+++ Documents/MIU/WAA/waa-august-2022-final-project/backend/src/main/java/com/example/backend/config/security/SecurityConfig.java
@@ -18,6 +18,12 @@ import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @KeycloakConfiguration
@@ -28,13 +34,21 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 )
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
+    private static final String[] ALLOWED_METHODS = {"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"};
     private static final String[] AUTH_WHITELIST = {"/swagger-resources/**", "/v2/api-docs/**", "/swagger.json",
             "/swagger-ui.html", "/webjars/**"};
+
+    private static final String[] ALLOWED_HEADERS = {
+            "accept", "cache-Control", "authorization", "content-type", "x-auth-token", "cookie", "set-cookie",
+            "content-disposition", "dnt", "if-modified-since", "keep-alive", "origin", "user-agent", "x-requested-with"
+    };
+    private static final long MAX_AGE = 86400L;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        http.csrf().disable().authorizeRequests()
+        http.cors().configurationSource(corsConfigurationSource())
+                .and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/api/resource/uploads/**").permitAll()
                 .antMatchers(AUTH_WHITELIST).permitAll()
                 .anyRequest().authenticated();
@@ -57,5 +71,18 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(buildRegistry());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList(ALLOWED_METHODS));
+        configuration.setAllowedHeaders(Arrays.asList(ALLOWED_HEADERS));
+        //        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(MAX_AGE);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
