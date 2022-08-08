5320418896456fdb55da587e2d3818cbc50753bd
bekmax <bekmax93@gmail.com>
2022-08-07 19:49:49 -0500

    Swagger documentation

--- Documents/MIU/WAA/waa-august-2022-final-project/backend/src/main/java/com/example/backend/controller/chart/ChartController.java
+++ Documents/MIU/WAA/waa-august-2022-final-project/backend/src/main/java/com/example/backend/controller/chart/ChartController.java
@@ -2,7 +2,10 @@ package com.example.backend.controller.chart;
 
 import com.example.backend.domain.State;
 import com.example.backend.dto.chart.LocationCountDto;
+import com.example.backend.dto.job.JobAdvertisementDto;
 import com.example.backend.service.chart.Charts;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,26 +17,31 @@ import java.util.List;
 @AllArgsConstructor
 @RestController
 @RequestMapping("api/charts")
+@ApiModel("Endpoint to provide chart data")
 public class ChartController {
 
     private final Charts charts;
 
     @GetMapping("/job-advertisement-count-by-location")
+    @ApiOperation(value = "Getting the list of Job advertisements per location", response = LocationCountDto.class, responseContainer = "List")
     public List<LocationCountDto> getJobAdvertisementsPerLocation() {
         return charts.getJobAdvertisementsPerLocation();
     }
 
     @GetMapping("/students-by-all-states")
+    @ApiOperation(value = "Getting the list of students per state", response = LocationCountDto.class, responseContainer = "List")
     public List<LocationCountDto> getStudentsByAllStates() {
         return charts.getStudentsByAllStates();
     }
 
     @GetMapping("/states/{name}/students-by-all-cities")
+    @ApiOperation(value = "Getting the list of students per city", response = LocationCountDto.class, responseContainer = "List")
     public List<LocationCountDto> getStudentsByAllCities(@PathVariable("name") State state) {
         return charts.getStudentsByAllCities(state);
     }
 
     @GetMapping("/tags-count-by-location")
+    @ApiOperation(value = "Getting the list of tags per location", response = LocationCountDto.class, responseContainer = "List")
     public List<LocationCountDto> getAllTagsByLocation() {
         return charts.getAllTagsByLocation();
     }
--- Documents/MIU/WAA/waa-august-2022-final-project/backend/src/main/java/com/example/backend/controller/user/CommentOnStudentController.java
+++ Documents/MIU/WAA/waa-august-2022-final-project/backend/src/main/java/com/example/backend/controller/user/CommentOnStudentController.java
@@ -1,8 +1,11 @@
 package com.example.backend.controller.user;
 
+import com.example.backend.dto.chart.LocationCountDto;
 import com.example.backend.dto.user.CommentOnStudentCreateRequest;
 import com.example.backend.dto.user.CommentOnStudentDto;
 import com.example.backend.service.user.CommentOnStudents;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,12 +18,14 @@ import javax.validation.Valid;
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/comments")
+@ApiModel("Endpoint to handle comments by faculty about students")
 public class CommentOnStudentController {
 
     private final CommentOnStudents commentOnStudents;
 
     @PostMapping
     @PreAuthorize("hasRole('ROLE_FACULTY')")
+    @ApiOperation(value = "Creating a comment by a faculty about a student", response = CommentOnStudentDto.class)
     public CommentOnStudentDto create(@Valid @RequestBody CommentOnStudentCreateRequest request) {
         return commentOnStudents.create(request);
     }
